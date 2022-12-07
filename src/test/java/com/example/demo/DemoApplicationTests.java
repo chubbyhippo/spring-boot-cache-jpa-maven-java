e4e822a3c5999766e43266e059591daad2ac5dbc
ch <mkmsacc@outlook.com>
2022-12-07 14:11:03 +0700

    Add integration test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,81 @@
 package com.example.demo;
 
+import com.example.demo.dto.StudentDto;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient(timeout = "36000")
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void shouldGetStudents() {
+        webTestClient.get()
+                .uri("/students")
+                .exchange()
+                .expectBodyList(StudentDto.class)
+                .hasSize(4);
+    }
+
+    @Test
+    @DirtiesContext
+    void shouldAddStudent() {
+        webTestClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/students")
+                        .queryParam("name", "Elena")
+                        .build())
+                .exchange()
+                .expectBodyList(StudentDto.class)
+                .hasSize(5);
+    }
+
+    @Test
+    void shouldNotGetStudentsMoreThanASecondAfterBeingCalled() {
+        webTestClient.get()
+                .uri("/students")
+                .exchange()
+                .expectBodyList(StudentDto.class)
+                .hasSize(4);
+
+        await().atMost(1L, TimeUnit.SECONDS)
+                .untilAsserted(() -> webTestClient.get()
+                        .uri("/students")
+                        .exchange()
+                        .expectBodyList(StudentDto.class)
+                        .hasSize(4));
+    }
+
+    @Test
+    @DirtiesContext
+    void shouldRefreshCacheAfterUpdating() {
+        webTestClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/students")
+                        .queryParam("name", "Elena")
+                        .build())
+                .exchange()
+                .expectBodyList(StudentDto.class)
+                .hasSize(5);
+
+        webTestClient.get()
+                .uri("/students")
+                .exchange()
+                .expectBodyList(StudentDto.class)
+                .hasSize(5);
+    }
 
 }
